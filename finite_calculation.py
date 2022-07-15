781857f5db0da1c973f600ee934ff803ce018fc4
zhxi969 <56410228+zhxi969@users.noreply.github.com>
2022-07-15 13:19:38 +0800

    “Learn finite calculation and elliptic curve”

---
+++ finite_calculation.py
@@ -0,0 +1,65 @@
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime  # <3>
+    # end::source1[]
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    # tag::source2[]
+    def __add__(self, other):
+        if self.prime != other.prime:  # <1>
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.num) % self.prime  # <2>
+        return self.__class__(num, self.prime)  # <3>
+    # end::source2[]
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num - other.num) % self.prime
+        # we return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num * other.num) % self.prime
+        # we return an element of the same class
+        return self.__class__(num, self.prime)
+
+    # tag::source3[]
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)  # <1>
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+    # end::source3[]
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        # use Fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        # we return an element of the same class
+        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+        return self.__class__(num, self.prime)
---
+++ point_on_elliptic _curve.py
@@ -0,0 +1,53 @@
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+
+        if self.x is None and self.y is None:  # <1>
+            return
+
+        if self.y**2 != self.x**3 + a * x + b:  # <1>
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):  # <2>
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+    # end::source1[]
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s ** 2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
+        if self == other:
+            s = (3 * self.x ** 2 + self.a) / (2 * self.y)
+            x = s ** 2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
